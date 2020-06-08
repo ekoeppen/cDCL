166ca101a61bc5d8e08648bea95bb2438aeab3d1
Paul Guyot <pguyot@kallisys.net>
2020-06-08 12:19:37 +0200

    Update toolchain usage and documentation

--- Sample_Code/ToolchainUtils/examples/PDemo.impl.s
+++ Sample_Code/ToolchainUtils/examples/PDemo.impl.s
@@ -1,29 +1,24 @@
 // =========== Header ===========
-// File:				PDemo.intf.s
-// Project:				DCL / ToolchainUtils
-// Written by:			Paul Guyot (pguyot@kallisys.net)
+// File:                PDemo.intf.s
+// Project:             DCL / ToolchainUtils
+// Written by:          Paul Guyot (pguyot@kallisys.net)
 //
-// Created on:			25/05/2020
-// Internal version:	1
+// Created on:          25/05/2020
+// Internal version:    1
 //
-// Copyright:			© 2020 by Paul Guyot.
-// 						All rights reserved worldwide.
+// Copyright:           © 2020 by Paul Guyot.
+//                      All rights reserved worldwide.
 // ===========
 
 // =========== Change History ===========
-// 25/05/2020	v1	[PG]	Creation of the file
+// 25/05/2020   v1  [PG]    Creation of the file
 // ===========
 
-	.arch armv3
+    .arch armv3
     .section .interp, "ax"
     . = 0
     .balign 4
     .global _ZN5PDemo9ClassInfoEv
-    .global _ZN5PDemo3NewEv
-    .global _ZN5PDemo6DeleteEv
-    .global _ZN5PDemo14GetConstStringERK6RefVar
-    .global _ZN5PDemo15GetArrayElementERK6RefVarS2_
-    .global _ZN5PDemo14BreakInMonitorERK6RefVar
 
     .global main
 
@@ -37,31 +32,31 @@ local___ZN5PDemo9ClassInfoEv:
     .word   __btbl__ - .
     .word   __monent__ - .
 @ branch to sizeof glue
-    B		_ZN5PDemo6SizeofEv
+    B       _ZN5PDemo6SizeofEv
 @ zero, or branch to Alloc method
-    .word	0
+    .word   0
 @ zero, or branch to Free method
     .word   0
 @ return, or branch to New(void)
-    B		_ZN5PDemo3NewEv
+    B       _ZN5PDemo3NewEv
 @ return, or branch to Delete
-	B		_ZN5PDemo6DeleteEv
+    B       _ZN5PDemo6DeleteEv
 @ version number
-    .word	0x00010000
+    .word   0x00010000
 @ flags
     .word   0x00000000
 @ (reserved)
     .word   0x00000000
 @ (reserved)
-	B		return__nil__5PDemo
+    B       return__nil__5PDemo
 
 _ZN5PDemo9ClassInfoEv:
-    adr		r0,local___ZN5PDemo9ClassInfoEv
-    mov		pc,r14
+    adr     r0,local___ZN5PDemo9ClassInfoEv
+    mov     pc,r14
 
 return__nil__5PDemo:
-    mov		r0,#0
-    mov		pc,r14
+    mov     r0,#0
+    mov     pc,r14
 
 __implname__:
     .string "PDemo"
--- Sample_Code/ToolchainUtils/examples/PDemoInterfaceGlue.s
+++ Sample_Code/ToolchainUtils/examples/PDemoInterfaceGlue.s
@@ -3,24 +3,38 @@
     .global _Z19AllocInstanceByNamePKcS0_
     .global _Z12FreeInstanceP9TProtocol
 
-    .macro Dispatch slotno
+    .macro _Dispatch slotno
     ldr     r0, [r0, #4]                // r0 -> "real" this
     ldr     r12, [r0, #8]               // r12 -> btable
     add     pc, r12, #\slotno*4         // go there
     .endm
 
-    .macro New classNameProc
+    .macro New symbol, className, slotno=2
+    .section ".text.\symbol\()", "ax"
+    .global \symbol
+    .balign 4
+asciz__\className\():
+    mov     r0, pc                      // r0 -> asciz name
+    mov     pc, r14                     // begone!
+    .string "\className\()"
+    .balign 4
+\symbol\():
     stmfd   r13!, {r0-r3, r14}          // save New(...) args
     mov     r1, r0                      // r1 = implementation name
-    bl      \classNameProc              // r0 = interface name
-    bl      _Z19AllocInstanceByNamePKcS0_ 
+    bl      asciz__\className           // r0 = interface name
+    bl      _Z19AllocInstanceByNamePKcS0_
     tst     r0, r0                      // success?
     add     r13, r13, #4                // pop (saved) string parm; r0 is 'this' now (or nil)
     ldmfd   r13!, {r1-r3, r14}          // pop New(...) args
     moveq   pc, r14                     // return nil if instance couldn't be made
+    _Dispatch   \slotno
     .endm
 
-    .macro Delete slotno
+    .macro Delete symbol, slotno=3
+    .section ".text.\symbol\()", "ax"
+    .global \symbol
+    .balign 4
+\symbol\():
     ldr     r0, [r0, #4]                // r0 -> real this
     stmfd   r13!, {r0, r14}             // save this, LK
     adr     r14, return                 // set return addr
@@ -31,45 +45,17 @@ return:
     b       _Z12FreeInstanceP9TProtocol // tail call to free mem
     .endm
 
-    .section ".text._ZN14PDemoInterface3NewEPc", "ax"
-    .global _ZN14PDemoInterface3NewEPc
+    .macro Dispatch symbol, slotno
+    .section ".text.\symbol\()", "ax"
+    .global \symbol
     .balign 4
+\symbol\():
+    _Dispatch    \slotno
+    .endm
 
-asciz__PDemoInterface:
-    mov     r0, pc                      // r0 -> asciz name
-    mov     pc, r14                     // begone!
-    .string "PDemoInterface"
-    .balign 4
+    New _ZN14PDemoInterface3NewEPc, PDemoInterface
+    Delete _ZN14PDemoInterface6DeleteEv
 
-_ZN14PDemoInterface3NewEPc:
-    New         asciz__PDemoInterface
-    Dispatch    2
-
-
-    .section ".text._ZN14PDemoInterface6DeleteEv", "ax"
-    .global _ZN14PDemoInterface6DeleteEv
-    .balign 4
-
-_ZN14PDemoInterface6DeleteEv:
-    Delete      3
-
-    .section ".text._ZN14PDemoInterface14GetConstStringERK6RefVar", "ax"
-    .global _ZN14PDemoInterface14GetConstStringERK6RefVar
-    .balign 4
-
-_ZN14PDemoInterface14GetConstStringERK6RefVar:
-    Dispatch    4
-
-    .section ".text._ZN14PDemoInterface15GetArrayElementERK6RefVarS2_", "ax"
-    .global _ZN14PDemoInterface15GetArrayElementERK6RefVarS2_
-    .balign 4
-
-_ZN14PDemoInterface15GetArrayElementERK6RefVarS2_:
-    Dispatch    5
-
-    .section ".text._ZN14PDemoInterface14BreakInMonitorERK6RefVar", "ax"
-    .global _ZN14PDemoInterface14BreakInMonitorERK6RefVar
-    .balign 4
-
-_ZN14PDemoInterface14BreakInMonitorERK6RefVar:
-    Dispatch    6
+    Dispatch    _ZN14PDemoInterface14GetConstStringERK6RefVar, 4
+    Dispatch    _ZN14PDemoInterface15GetArrayElementERK6RefVarS2_, 5
+    Dispatch    _ZN14PDemoInterface14BreakInMonitorERK6RefVar, 6
